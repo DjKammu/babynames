c61a5e57d693ee4221f51ed7714bc1e407224ea3
Kamlesh DjKammu <dhundharakamlesh@gmail.com>
2022-01-07 16:01:41 +0530

    WIP

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>404 Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+</head>
+
+<body>
+  <div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <span class="display-1 d-block">404</span>
+                <div class="mb-4 lead">The page you are looking for was not found.</div>
+                <a href="{{ url('/')}}" class="btn btn-link">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</div>
+<style type="text/css">
+body {
+background: #dedede;
+}
+.page-wrap {
+min-height: 100vh;
+}
+
+</style>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -27,36 +27,36 @@ Route::get('/baby-names/{cat}/{gender}/{letter}','NameController@getNames')
 Route::get('/baby-names/{cat}/{gender}/{term}/{word}','NameController@searchNames')
 ->name('baby-names.search');
 
-Route::get('/cache-clear', function () {
-    Artisan::call('config:clear');
-    Artisan::call('cache:clear');
-    Artisan::call('view:clear');
-});
+// Route::get('/cache-clear', function () {
+//     Artisan::call('config:clear');
+//     Artisan::call('cache:clear');
+//     Artisan::call('view:clear');
+// });
 
-Route::get('search/{slug}.xml', [
-  'uses' => 'SitemapController@search'
-])->where('slug','.*');
+// Route::get('search/{slug}.xml', [
+//   'uses' => 'SitemapController@search'
+// ])->where('slug','.*');
 
-Route::get('search/{slug}', [
-  'uses' => 'NameController@search'
-])->where('slug','.*');
-
-
-Route::get('{tc}/{slug}', [
-  'uses' => 'NameController@tagOrigin'
-])->where('slug', '([A-Za-z0-9\-\/]+)');
+// Route::get('search/{slug}', [
+//   'uses' => 'NameController@search'
+// ])->where('slug','.*');
 
 
-Route::get('{slug}', [
-  'uses' => 'NameController@lyrichord'
-])->where('slug', '([A-Za-z0-9\-\/]+)');
+// Route::get('{tc}/{slug}', [
+//   'uses' => 'NameController@tagOrigin'
+// ])->where('slug', '([A-Za-z0-9\-\/]+)');
 
 
-/*SITE MAP */
-Route::get('/sitemap/index.xml', 'SitemapController@index');
-Route::get('/sitemap/categories.xml', 'SitemapController@categories');
-Route::get('/sitemap/tags.xml', 'SitemapController@tags');
-Route::get('/sitemap/origins.xml', 'SitemapController@origins');
+// Route::get('{slug}', [
+//   'uses' => 'NameController@lyrichord'
+// ])->where('slug', '([A-Za-z0-9\-\/]+)');
+
+
+// /*SITE MAP */
+// Route::get('/sitemap/index.xml', 'SitemapController@index');
+// Route::get('/sitemap/categories.xml', 'SitemapController@categories');
+// Route::get('/sitemap/tags.xml', 'SitemapController@tags');
+// Route::get('/sitemap/origins.xml', 'SitemapController@origins');
 
 // Route::get('{tc}.xml', [
 //   'uses' => 'SitemapController@tagsCateogries'
